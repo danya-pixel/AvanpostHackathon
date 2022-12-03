e9c00aa4639a8d135890f97b9096ea4f0d2910d6
danya-pixel <exisionmonkey@gmail.com>
2022-12-03 18:09:51 +0700

    add finetune

--- Finetuner.py
+++ Finetuner.py
@@ -1,6 +1,80 @@
-class Finetuner():
-    def __init__(self) -> None:
-        self.pth_path = pth_path
-        self.class_dict = class_dict
-        self.images_path = images_path
-        
+import json
+from dataset_working import get_dataloaders, get_dataloader_pred
+import torch
+from torch import nn
+from train import train_model_default
+import torchvision
+import numpy as np
+
+
+def get_classes_dict(classes_names):
+    return dict(classes_names, range(len(classes_names)))
+
+def finetune_model(classes_names, pth_path, new_data_dir, new_data_name):
+    np.random.seed(42)
+    torch.seed()
+    class_names.append(new_data_name)
+
+    device = torch.device("cuda:1") if torch.cuda.is_available() else torch.device("cpu")
+    torch.cuda.set_device(1)
+
+    data_dir = 'dataset'
+    classes = get_classes_dict(classes_names)
+    class_names = {v: k for k, v in classes.items()}
+
+    dataloaders = get_dataloaders(data_dir, classes, new_data_dir, new_data_name)
+    print(f"Размер трейн даталоадера: {len(dataloaders['train'])}")
+    model_ft = torchvision.models.resnet18()
+    model_ft.load_state_dict(torch.load(pth_path))
+    print('Модель загружена')
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, len(classes_names))
+
+    model_ft = model_ft.to(device)
+
+    criterion = nn.CrossEntropyLoss()
+
+    optimizer_ft = torch.optim.SGD(model_ft.parameters(), lr=0.001, momentum=0.9)
+    exp_lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer_ft, step_size=7, gamma=0.1)
+
+    model_ft = train_model_default(model_ft, device, dataloaders, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=25)
+
+
+
+def predict_samples(classes_names, pth_path, new_data_dir):
+    np.random.seed(42)
+    torch.seed()
+    meta = {}
+
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    model_ft = torchvision.models.resnet18()
+    num_ftrs = model_ft.fc.in_features
+    model_ft.fc = nn.Linear(num_ftrs, len(classes_names))
+    print(device)
+    model_ft.load_state_dict(torch.load(pth_path, map_location=device))
+    model_ft.to(device)
+    
+    
+    print('Модель загружена')
+   
+    
+
+    model_ft.eval()
+    dataloader = get_dataloader_pred(new_data_dir)
+    
+    with torch.no_grad():
+        for i, (inputs, path) in enumerate(dataloader):
+                    inputs = inputs.to(device)
+
+                    outputs = model_ft(inputs)
+                    _, preds = torch.max(outputs, 1)
+                    for pred, file in zip(preds, path):
+                        meta[file] = int(pred.detach().cpu())
+    return meta
+    
+
+   
+    
+
+
+
--- dataset_working.py
+++ dataset_working.py
@@ -1,9 +1,10 @@
 from typing import Dict
 from torch.utils.data import Dataset, DataLoader
-
+import torchvision.transforms as transforms
 from sklearn.model_selection import train_test_split
 
 from PIL import Image
+import PIL
 import torch
 import os
 import glob
@@ -20,38 +21,117 @@ import pandas as pd
 
 class ImageNetDataset(Dataset):
     def __init__(
-        self, data_dir, file_lists, labels_set, transform = None):
+        self, file_lists, labels_set, transform = None):
 
-        
-        # all_files = glob.glob(data_dir + '/**/*', recursive=True)
-        # self.all_files_paths = [file for file in all_files if len(os.path.normpath(file).split(os.sep)) == 3]
-        # self.all_files_sep = [os.path.normpath(file).split(os.sep) for file in self.all_files_paths]
-        self.data_dir = data_dir
-        self.all_files_names = list(file_lists['X_name'])
-        self.all_files_dirs = list(file_lists['Y_name'])
-        self.all_files_labels = list(file_lists['Y'])
-
-
+        self.files = list(file_lists['x'])
+        self.labels = list(file_lists['y'])
         self.labels_set = labels_set
         self.transform = transform
 
     def __len__(self):
-        return len(self.all_files_names)
+        return len(self.files)
     
     def __getitem__(self, index):
-        #img_path = self.all_files_paths[index]
-        img_path = os.path.join(self.data_dir, self.all_files_dirs[index], self.all_files_names[index])
+        img_path = self.files[index]
         img = Image.open(img_path).convert('RGB')
 
         if self.transform is not None:
             img = self.transform(img)
 
         #label = self.labels_set[self.all_files_sep[index][-2]]
-        label = self.all_files_labels[index]
+        label = self.labels[index]
         return img, label
+
+class ImageNetDatasetPred(Dataset):
+    def __init__(
+        self, file_lists, transform = None):
+
+        self.files = list(file_lists['x'])
+        self.labels = list(file_lists['y'])
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.files)
+    
+    def __getitem__(self, index):
+        img_path = self.files[index]
+        img = Image.open(img_path).convert('RGB')
+
+        if self.transform is not None:
+            img = self.transform(img)
+        return img, img_path
         
 
+def get_all_files(data_dir):
+    data_paths = []
+    data_labels = []
+    for r, _, f in os.walk(data_dir):
+        for name in f:
+            data_paths.append(os.path.join(r, name))
+            data_labels.append(r.split('/')[1])
+    return data_paths, data_labels
+
+def get_dataloader_pred(data_dir):
+    normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+)
+    input_transform = transforms.Compose([
+        transforms.Resize(256, PIL.Image.BICUBIC),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    new_files, _ = get_all_files(data_dir=data_dir)
+    new_lables = 0 * len(new_files)
+    init_dataframe = pd.DataFrame({'x': new_files, 'y': new_lables})
+    dataset_pred = ImageNetDatasetPred(init_dataframe, transform=input_transform)
+    dataloader_pred = torch.utils.data.DataLoader(dataset_pred, batch_size=64, shuffle=True, num_workers=4)
+    return dataloader_pred
+
+def get_dataloaders(data_dir, classes, new_data_dir, new_data_name):
+    normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+)
+    input_transform = transforms.Compose([
+        transforms.Resize(256, PIL.Image.BICUBIC),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    init_files, init_labels = get_all_files(data_dir=data_dir)
+    init_labels = [classes[i] for i in init_labels]
+    new_files, _ = get_all_files(data_dir=new_data_dir)
+    new_labels = classes[new_data_name] * len(new_files)
+
+    init_dataframe = pd.DataFrame({'x': init_files, 'y': init_labels})
+    new_dataframe = pd.DataFrame({'x': new_files, 'y':new_labels})
+    dataset_list = pd.concat([init_dataframe, new_dataframe])
+
+    train_data, val_test_data = train_test_split(dataset_list, test_size = 0.2, shuffle = True)
+    val_data, test_data = train_test_split(val_test_data, test_size = 0.5, shuffle = True)
+
+    dataset_train = ImageNetDataset(train_data, classes, transform = input_transform)
+    dataset_val = ImageNetDataset(val_data, classes, transform = input_transform)
+    dataset_test = ImageNetDataset(test_data, classes, transform = input_transform)
 
 
+    image_datasets = {'train':dataset_train, 'val':dataset_val, 'test':dataset_test} #тут надо вставить датасеты
 
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
+                                                shuffle=True, num_workers=4)
+                for x in ['train', 'val', 'test']}
+    
+    return dataloaders
 
+def predict_dataloader(new_data_paths):
+    normalize = transforms.Normalize(
+    mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]
+)
+    input_transform = transforms.Compose([
+        transforms.Resize(256, PIL.Image.BICUBIC),
+        transforms.CenterCrop(224),
+        transforms.ToTensor(),
+        normalize,
+    ])
+    dataset_list = pd.read_csv(new_data_paths)
+    dataset_pred = ImageNetDataset(data_dir, train_data, classes, transform = input_transform)
