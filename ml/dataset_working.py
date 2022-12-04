03fc34e5f724d7c880dd0a998325011d942427fe
danya-pixel <exisionmonkey@gmail.com>
2022-12-04 07:19:36 +0000

    new finetune resnet50

--- ml/dataset_working.py
+++ ml/dataset_working.py
@@ -69,6 +69,8 @@ def get_all_files(data_dir):
         for name in f:
             if '.gitkeep' == name:
                 continue
+            if name.startswith('.'):
+                continue
             name = Path(name)
             r = Path(r)
             filepath = r/name
@@ -117,6 +119,32 @@ def get_dataloaders(data_dir, classes, new_data_dir, new_data_name):
 
     dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
                                                   shuffle=True, num_workers=0)
+                   for x in ['train', 'val', 'test']}
+
+    return dataloaders
+
+def get_dataloaders_train(data_dir, classes):
+    init_files, init_labels = get_all_files(data_dir=data_dir)
+    init_labels = [classes[i] for i in init_labels]
+
+    dataset_list = pd.DataFrame({'x': init_files, 'y': init_labels})
+
+    train_data, val_test_data = train_test_split(
+        dataset_list, test_size=0.2, shuffle=True)
+    val_data, test_data = train_test_split(
+        val_test_data, test_size=0.5, shuffle=True)
+
+    dataset_train = ImageNetDataset(
+        train_data, classes, transform=get_train_transforms())
+    dataset_val = ImageNetDataset(val_data, classes, transform=get_train_transforms())
+    dataset_test = ImageNetDataset(
+        test_data, classes, transform=get_test_transforms())
+
+    image_datasets = {'train': dataset_train, 'val': dataset_val,
+                      'test': dataset_test}  # тут надо вставить датасеты
+
+    dataloaders = {x: torch.utils.data.DataLoader(image_datasets[x], batch_size=64,
+                                                  shuffle=True, num_workers=32)
                    for x in ['train', 'val', 'test']}
 
     return dataloaders
--- ml/finetuner.py
+++ ml/finetuner.py
@@ -5,13 +5,13 @@ import torch
 
 from dataset_working import get_dataloaders, get_dataloader_pred
 from test import test_model
-from train import train_model_default
+from train import train_model_tune
 
 
 def get_classes_dict(classes_names):
     return dict(zip(classes_names, list(range(len(classes_names)))))
 
-def finetune_model(data_dir, classes_names, pth_path, new_data_dir, new_data_name):
+def finetune_model(data_dir, classes_names, pth_path, new_data_dir, new_data_name, ood_data_dir = None, ood_data_name = None):
     meta = dict()
     np.random.seed(42)
     torch.seed()
@@ -25,7 +25,7 @@ def finetune_model(data_dir, classes_names, pth_path, new_data_dir, new_data_nam
 
     classes = get_classes_dict(classes_names)
 
-    dataloaders = get_dataloaders(data_dir, classes, new_data_dir, new_data_name)
+    dataloaders = dataloaders = get_dataloaders(data_dir, classes, new_data_dir, new_data_name) 
 
     criterion = torch.nn.CrossEntropyLoss()
 
@@ -34,16 +34,20 @@ def finetune_model(data_dir, classes_names, pth_path, new_data_dir, new_data_nam
     for name, param in model_ft.named_parameters():
         if not 'fc' in name:
             param.requires_grad = False
-    model_ft = train_model_default(meta, model_ft, device, dataloaders, criterion, optimizer_ft, exp_lr_scheduler, num_epochs= 20, model_path=meta['pth_path'])
+    model_ft = train_model_tune(meta, model_ft, device, dataloaders, criterion, optimizer_ft, exp_lr_scheduler, num_epochs=5, model_path=meta['pth_path'])
+
+
     acc, f1 = test_model(model_ft, dataloaders['test'], device)
     meta['acc'], meta['f1'] = float(acc), float(f1)
     return meta
+
+
 
 def load_model(classes_names, pth_path, device):
     np.random.seed(42)
     torch.seed()
 
-    model_ft = torchvision.models.resnet18()
+    model_ft = torchvision.models.resnet50()
     num_ftrs = model_ft.fc.in_features
     model_ft.fc = torch.nn.Linear(num_ftrs, len(classes_names))
     model_ft.load_state_dict(torch.load(pth_path, map_location=device))
@@ -54,7 +58,7 @@ def load_model_finetune(classes_names, pth_path, device):
     np.random.seed(42)
     torch.seed()
 
-    model_ft = torchvision.models.resnet18()
+    model_ft = torchvision.models.resnet50()
     num_ftrs = model_ft.fc.in_features
     model_ft.fc = torch.nn.Linear(num_ftrs, len(classes_names))
     model_ft.load_state_dict(torch.load(pth_path, map_location=device))
@@ -63,6 +67,44 @@ def load_model_finetune(classes_names, pth_path, device):
     return model_ft
 
 def predict_samples(classes_names, pth_path, new_data_dir):
+    best_thresholds = [0.3,
+  0.3,
+  0.3,
+  0.3,
+  0.3,
+  0.3,
+  0.3,
+  0.3,
+  0.3,
+  0.3]
+    np.random.seed(42)
+    torch.seed()
+    meta = {}
+    softmax = torch.nn.Softmax(dim=1)
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+    model_ft = load_model(classes_names, pth_path, device)
+    model_ft.eval()
+    dataloader = get_dataloader_pred(new_data_dir)
+    
+    with torch.no_grad():
+        for i, (inputs, path) in enumerate(dataloader):
+                    inputs = inputs.to(device)
+
+                    outputs = model_ft(inputs)
+                    sf_out = softmax(outputs).cpu()
+                    preds = torch.tensor(
+                    [
+                    -1 if (p < best_thresholds).all() else p.argmax()
+                    for p in sf_out.numpy()
+                    ]
+            )
+                    # _, preds = torch.max(outputs, 1)
+                    # print(outputs.detach().cpu())
+                    for pred, file in zip(preds, path):
+                        meta[file] = int(pred.detach().cpu())
+    return meta
+    
+def predict_samples_old(classes_names, pth_path, new_data_dir):
     np.random.seed(42)
     torch.seed()
     meta = {}
@@ -80,8 +122,6 @@ def predict_samples(classes_names, pth_path, new_data_dir):
                     for pred, file in zip(preds, path):
                         meta[file] = int(pred.detach().cpu())
     return meta
-    
-
    
     
 
